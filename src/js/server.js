7c5588939f7b2f9490ec0862866e793b4f483134
Grace Lee <eunheh@gmail.com>
2016-11-22 20:48:23 -0500

    new directory

--- src/js/server.js
+++ src/js/server.js
@@ -1,3 +1,3 @@
-const SERVER = 'https://class-server.herokuapp.com/collections/gl-detail-contact/';
+const SERVER = 'https://class-server.herokuapp.com/collections/glee-detail-contact/';
 
 export { SERVER };
